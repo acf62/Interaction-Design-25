08f0f00190144c7f84b93f163d4d43714276241a
acf62 <acf62@cam.ac.uk>
2019-05-16 15:26:28 +0100

    Basic UI

---
+++ src/uk/ac/cam/interactiondesign25/main/Controller.java
@@ -0,0 +1,54 @@
+package uk.ac.cam.interactiondesign25.main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+
+public class Controller {
+
+    @FXML
+    private ImageView image1;
+    @FXML
+    private ImageView image2;
+    @FXML
+    private ImageView image3;
+    @FXML
+    private ImageView image4;
+    @FXML
+    private ImageView image5;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    private Button dayButton;
+
+    @FXML
+    private Button locationButton;
+
+    @FXML
+    private Button weekButton;
+
+    @FXML
+    void settingsClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dayClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void weekClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void locationClick(ActionEvent event) {
+
+    }
+
+}
--- src/uk/ac/cam/interactiondesign25/main/Main.java
+++ src/uk/ac/cam/interactiondesign25/main/Main.java
@@ -1,4 +1,32 @@
 package uk.ac.cam.interactiondesign25.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main_scene.fxml"));
+        primaryStage.setTitle("Weather App");
+        //String IconPath = getClass().getResource("/icon.png").toString();
+        //primaryStage.getIcons().add(new Image(IconPath));
+        Scene scene = new Scene(root, 1150, 750);
+        //String StyleSheetsPath = getClass().getResource("/style.css").toString();
+        //scene.getStylesheets().add(StyleSheetsPath);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
