8e59dc8e4e3fbd6247f62e29f704f84d5dd7a62e
Dom Henderson <dominic.henderson1@outlook.com>
2019-05-16 11:43:13 +0100

    Added weather code conversion

--- src/uk/ac/cam/interactiondesign25/api/WeatherType.java
+++ src/uk/ac/cam/interactiondesign25/api/WeatherType.java
@@ -1,11 +1,32 @@
 package uk.ac.cam.interactiondesign25.api;
 
 public enum WeatherType {
-    UNKNOWN,            // NA
+	
+	UNKNOWN,            // NA
     SUNNY,              // 1
     PARTIALLY_CLOUDY,   // 2, 3, 5
     CLOUDY,             // 6-8
     RAINY,              // 9-18
     SNOWY,              // 19-27
-    THUNDER             // 28-30
+    THUNDER;            // 28-30
+	
+	public static WeatherType convert ( int code ) {
+		if ( code <= 0 ) {
+			return WeatherType.UNKNOWN;
+		} else if ( code == 1 ) {
+			return WeatherType.SUNNY;
+		} else if ( code <= 5 ) {
+			return WeatherType.PARTIALLY_CLOUDY;
+		} else if ( code <= 8 ) {
+			return WeatherType.CLOUDY;
+		} else if ( code <= 18 ) {
+			return WeatherType.RAINY;
+		} else if ( code <= 27 ) {
+			return WeatherType.SNOWY;
+		} else if ( code <= 30 ) {
+			return WeatherType.THUNDER;
+		} else {
+			return WeatherType.UNKNOWN;
+		}
+	}
 }
