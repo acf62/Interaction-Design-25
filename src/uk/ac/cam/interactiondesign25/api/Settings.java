d2ee398fd276e0068121fcea6d7a65ecc440d5d2
Dan <djh242@cam.ac.uk>
2019-05-16 14:12:22 +0100

    Add settings functionality

--- src/uk/ac/cam/interactiondesign25/api/Settings.java
+++ src/uk/ac/cam/interactiondesign25/api/Settings.java
@@ -1,29 +1,59 @@
 package uk.ac.cam.interactiondesign25.api;
 
+import java.io.*;
+
 public class Settings {
+    private String filename;
     private boolean Celcius;
     private boolean BlueYellowColourblind;
     private boolean RedGreenColourblind;
 
-    public Settings(String filename){
-        // TODO
-        // if filename doesn't exist, create it, write default data to it
-        // otherwise, load it into variables
+    private void writeBack(){
+        try (FileWriter f = new FileWriter(filename)) {
+            BufferedWriter w = new BufferedWriter(f);
+            w.write(((Celcius)?"1":"0")+"\n");
+            w.write(((BlueYellowColourblind)?"1":"0")+"\n");
+            w.write(((RedGreenColourblind)?"1":"0")+"\n");
+            w.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Settings(String fn){
+        filename = fn;
+        try (FileReader f = new FileReader(filename)){
+            // Load file contents into variables
+            BufferedReader r = new BufferedReader(f);
+            Celcius = r.readLine().equals("1");
+            BlueYellowColourblind = r.readLine().equals("1");
+            RedGreenColourblind = r.readLine().equals("1");
+
+        } catch (FileNotFoundException e1) {
+            // Initialise file - didn't exist
+            Celcius = true;
+            BlueYellowColourblind = false;
+            RedGreenColourblind = false;
+            writeBack();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setCelcius(boolean val){
         Celcius = val;
-        // TODO Update in file also
+        writeBack();
     }
 
     public void setBlueYellowColourblind(boolean val){
         BlueYellowColourblind = val;
-        // TODO Update in file also
+        writeBack();
     }
 
     public void setRedGreenColourblind(boolean val){
         RedGreenColourblind = val;
-        // TODO Update in file also
+        writeBack();
     }
 
     public boolean getCelcius(){
