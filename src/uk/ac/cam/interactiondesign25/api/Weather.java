662146e2f70d920bff7ee02d0c61d9369ef44085
Dan <djh242@cam.ac.uk>
2019-05-16 15:20:34 +0100

    Write getTodayThreeHourlyTemperatures

--- src/uk/ac/cam/interactiondesign25/api/Weather.java
+++ src/uk/ac/cam/interactiondesign25/api/Weather.java
@@ -6,18 +6,20 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Calendar;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import static uk.ac.cam.interactiondesign25.api.TemperatureConversion.toFahrenheit;
 
 public class Weather {
-    private Settings settings;
-    private int locationID;
+	private Settings settings;
+	private int locationID;
 	
 	private final String baseUrl = "http://datapoint.metoffice.gov.uk/public/data/";
 	private final String apiKey = "5887b42a-ab8e-4285-94e8-9ef8ca4fe411";
-	private final boolean active = false; //Used during development to reduce number of API calls
+	private final boolean active = true; //Used during development to reduce number of API calls
 	
 	private final long updateTime = 600; //update time in seconds
 
@@ -27,16 +29,16 @@ public class Weather {
 	
 	private long lastUpdateTime = 0;
 	
-    public Weather(String settingsFilename){
-        settings = new Settings(settingsFilename);
-        locationID = 310042; // default to cambridge
+	public Weather(String settingsFilename){
+		settings = new Settings(settingsFilename);
+		locationID = 310042; // default to cambridge
 		if ( active) {
 			doAPICallIfNecessary();
 		}
-    }
+	}
 
-    // Returns today's maximum and minimum temperatures
-    public int[] getTodayTemperatures(){
+	// Returns today's maximum and minimum temperatures
+	public int[] getTodayTemperatures(){
 		int[] result = {0,0};
 		if ( active ) {
 			doAPICallIfNecessary();
@@ -58,41 +60,51 @@ public class Weather {
 			}
 		}
 		return result;
-    }
+	}
 
-    // Returns today's 3-hourly temperature forecasts
-    public int[] getTodayThreeHourlyTemperatures(){
-        int[] result = {0,0,0,0,0};
+	// Returns today's 3-hourly temperature forecasts
+	public int[] getTodayThreeHourlyTemperatures(){
+		int[] result = {0,0,0,0,0};
 		if ( active ) {
 			doAPICallIfNecessary();
-			JSONObject weatherObject = new JSONObject ( ThreeHourlyForecast );
-			JSONObject DV = weatherObject.getJSONObject("SiteRep").getJSONObject("DV");
-			JSONObject Location = DV.getJSONObject("Location");
-			JSONArray Period = Location.getJSONArray("Period");
-			
-			for ( int i = 0; i < Period.length(); ++i ) {
-				JSONObject j = Period.getJSONObject(i);
-				JSONArray a = j.getJSONArray("Rep");
-				JSONObject day = a.getJSONObject(0);
-				result[i] = day.getInt("Dm");
+			JSONArray Period = new JSONObject ( threeHourlyForecast )
+					.getJSONObject("SiteRep")
+					.getJSONObject("DV")
+					.getJSONObject("Location")
+					.getJSONArray("Period");
+
+			int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+			int chunksLeftToday = (26-currentHour)/3;
+			int chunksFromTomorrow = 5 - chunksLeftToday;
+
+			for(int i=0; i<chunksLeftToday; i++){
+				JSONArray rep = Period
+						.getJSONObject(0) //today
+						.getJSONArray("Rep");
+				result[i] = rep
+						.getJSONObject(rep.length() - chunksLeftToday + i)
+						.getInt("T");
+			}
+
+			for(int i=0; i<chunksFromTomorrow; i++){
+				JSONArray rep = Period
+						.getJSONObject(1) //tomorrow
+						.getJSONArray("Rep");
+				result[i + chunksLeftToday] = rep
+						.getJSONObject(i)
+						.getInt("T");
 			}
 		}
 		return result;
-    }
-
-	public static void main(String[] args){
-		// for testing purposes
-		Weather w = new Weather("settings");
-		w.getTodayThreeHourlyTemperatures();
 	}
 
 	// Returns a textual description of today's weather
-    public String getTodayWeatherDescription(){
-        return "Cloudy and mildly depressing";
-    }
+	public String getTodayWeatherDescription(){
+		return "Cloudy and mildly depressing";
+	}
 
-    // Returns type of today's weather (for use in selecting icons)
-    public WeatherType getTodayWeatherType(){
+	// Returns type of today's weather (for use in selecting icons)
+	public WeatherType getTodayWeatherType(){
 		if ( active ) {
 			doAPICallIfNecessary();
 			JSONObject weatherObject = new JSONObject ( weeklyForecast );
@@ -108,11 +120,11 @@ public class Weather {
 		} else {
 			return WeatherType.UNKNOWN;
 		}
-    }
+	}
 
-    // Returns high and low temperatures for each day for the next week,
-    // starting with today
-    public int[][] getWeekTemperatures(){
+	// Returns high and low temperatures for each day for the next week,
+	// starting with today
+	public int[][] getWeekTemperatures(){
 		int[][] result = {{0,0}, {0,0}, {0,0}, {0,0}, {0,0}};
 		if ( active ) {
 			doAPICallIfNecessary();
@@ -138,23 +150,23 @@ public class Weather {
 			}
 		}
 		return result;
-    }
+	}
 
-    // Returns textual descriptions of the weather each day this week,
-    // starting with today
-    public String[] getWeekWeatherDescription(){
-        return new String[]{"", "", "", "", ""};
-    }
+	// Returns textual descriptions of the weather each day this week,
+	// starting with today
+	public String[] getWeekWeatherDescription(){
+		return new String[]{"", "", "", "", ""};
+	}
 
-    // Returns types of the weather each day this week, starting with today
-    public WeatherType[] getWeekWeatherTypes(){
+	// Returns types of the weather each day this week, starting with today
+	public WeatherType[] getWeekWeatherTypes(){
 		WeatherType[] result = {
-                WeatherType.UNKNOWN,
-                WeatherType.UNKNOWN,
-                WeatherType.UNKNOWN,
-                WeatherType.UNKNOWN,
-                WeatherType.UNKNOWN,
-        };
+				WeatherType.UNKNOWN,
+				WeatherType.UNKNOWN,
+				WeatherType.UNKNOWN,
+				WeatherType.UNKNOWN,
+				WeatherType.UNKNOWN,
+		};
 		if ( active ) {
 			doAPICallIfNecessary();
 			JSONObject weatherObject = new JSONObject ( weeklyForecast );
@@ -172,24 +184,25 @@ public class Weather {
 		}
 		return result;
 
-    }
+	}
 
-    // Calls the API and caches the results locally unless already have data
-    private void doAPICallIfNecessary(){
-        if (!haveData()){
-            downloadWeeklyForecast();
-            downloadThreeHourlyForecast();
+	// Calls the API and caches the results locally unless already have data
+	private void doAPICallIfNecessary(){
+		if (!haveData()){
+			downloadWeeklyForecast();
+			downloadThreeHourlyForecast();
+			lastUpdateTime = System.currentTimeMillis();
 			System.out.println ( "Did API calls" );
-        } else {
+		} else {
 			System.out.println ( "Used cache" );
 		}
-    }
+	}
 
-    // Returns true if we already have up-to-date data for the location
-    private boolean haveData(){
+	// Returns true if we already have up-to-date data for the location
+	private boolean haveData(){
 		//lastUpdateTime starts at 0 and currentTimeMillis is since 1970 so this will pass on first try
-		return System.currentTimeMillis() - lastUpdateTime >= 1000*updateTime;
-    }
+		return System.currentTimeMillis() - lastUpdateTime < 1000*updateTime;
+	}
 	
 	private void downloadWeeklyForecast() {
 		String result = "";
@@ -236,8 +249,8 @@ public class Weather {
 		}
 	}
 
-    // Checks user settings for whether they use Celcius or Fahrenheit
-    private boolean useCelcius(){
-        return settings.getCelcius();
-    }
+	// Checks user settings for whether they use Celcius or Fahrenheit
+	private boolean useCelcius(){
+		return settings.getCelcius();
+	}
 }
