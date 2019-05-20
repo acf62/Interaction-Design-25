234edf4ce079d9299e31df6698212f6ceea57ca0
HaskarP <harinuno1@gmail.com>
2019-05-20 15:50:23 +0100

    Changed Main

--- src/uk/ac/cam/interactiondesign25/main/Main.java
+++ src/uk/ac/cam/interactiondesign25/main/Main.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import uk.ac.cam.interactiondesign25.api.Settings;
 import uk.ac.cam.interactiondesign25.api.Weather;
 
 import java.io.File;
@@ -16,27 +17,35 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    public static Weather weather = new Weather("settingsFile");
+    public static Weather weather;
 
     public static Stage mainStage;
 
     public static Scene locationScene, dayScene, settingsScene, weekScene;
 
+    public static Settings settings;
 
     public static WeekController weekController;
     public static LocationController locationController;
+    public static SettingsController settingsController;
 
     static {
         try {
+            weather = new Weather("settingsFile");
+            settings = new Settings("settingsFile");
             FXMLLoader fxmlLoader;
-            fxmlLoader = new FXMLLoader(new File("C:\\Users\\Hari\\Desktop\\Year 2\\InteractionDesign\\Interaction-Design-25\\resources\\locationscreen.fxml").toURI().toURL());
+            fxmlLoader = new FXMLLoader(new File("resources/locationscreen.fxml").toURI().toURL());
             locationScene = new Scene(fxmlLoader.load(),1365,768);
             locationController = fxmlLoader.getController();
-            fxmlLoader = new FXMLLoader(new File("C:\\Users\\Hari\\Desktop\\Year 2\\InteractionDesign\\Interaction-Design-25\\resources\\week_scene.fxml").toURI().toURL());
+            fxmlLoader = new FXMLLoader(new File("resources/weekscreen.fxml").toURI().toURL());
             weekScene = new Scene(fxmlLoader.load(),1365,768);
             weekController = fxmlLoader.getController();
-            System.out.println(locationController== null);
+            fxmlLoader = new FXMLLoader(new File("resources/settings.fxml").toURI().toURL());
+            settingsScene = new Scene(fxmlLoader.load(),1365,768);
+            settingsController = fxmlLoader.getController();
+            /*System.out.println(locationController== null);
             System.out.println (weekController== null);
+            System.out.println(settingsController==null);*/
         }
         catch (Exception e) {
             System.out.println("Exception in Static Initializer of main");
@@ -44,8 +53,6 @@ public class Main extends Application {
         }
 
     }
-
-
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -73,11 +80,18 @@ public class Main extends Application {
     public static void receive(String instr) {
         if (instr.equals("week")) {
             mainStage.setScene(weekScene);
-            System.out.println(weekController==null);
             weekController.sync();
         }
+
         if (instr.equals("location")) {
             mainStage.setScene(locationScene);
+        }
+        else if (instr.equals("settings")) {
+            mainStage.setScene(settingsScene);
+            settingsController.sync();
+        }
+        else {
+            System.out.println("Received incorrect screen");
         }
 
     }
